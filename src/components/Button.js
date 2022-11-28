83fa472029bfb836a5169db2a084b83506c3e898
Sam Maya <samuelmaya112@gmail.com>
2022-11-28 15:49:41 -0500

    finishing Touches

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
-const Button = ({color, text, onClick, showAdd}) => {
+const Button = ({color, text, onClick}) => {
   return (
-    <button style={{backgroundColor: color}} className ='btn' onClick={onClick} showAdd={showAdd}>{text}</button>
+    <button style={{backgroundColor: color}} className ='btn' onClick={onClick}>{text}</button>
   )
 }
 
